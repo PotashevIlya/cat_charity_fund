1d4a04a4a6f4ffad6f48cc74de020b5e420b59be
Илья Поташев <potaschev.ilya@yandex.ru>
2025-01-13 19:56:34 +0300

    created test get and post endpoints for charity project

---
+++ app/api/endpoints/charity_project.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Depends
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.db import get_async_session
+from app.core.user import current_superuser, current_user
+from app.crud.charity_project import charity_project_crud
+from app.schemas.charity_project import (
+    CharityProjectCreate, CharityProjectDB, CharityProjectUpdate
+)
+
+router = APIRouter()
+
+
+@router.post(
+    '/',
+    response_model=CharityProjectDB,
+    response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)],
+)
+async def create_new_charity_project(
+    charity_project: CharityProjectCreate,
+    session: AsyncSession = Depends(get_async_session),
+):
+    """Только для суперюзеров."""
+    new_charity_project = await charity_project_crud.create(
+        charity_project,
+        session
+    )
+    return new_charity_project
+
+
+@router.get(
+    '/',
+    response_model=list[CharityProjectDB],
+    response_model_exclude_none=True,
+)
+async def get_all_charity_projects(
+    session: AsyncSession = Depends(get_async_session)
+):
+    """Для любого пользователя."""
+    all_charity_projects = await charity_project_crud.get_multi(session)
+    return all_charity_projects
--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import user_router
+from app.api.endpoints import user_router, charity_project_router
 
 
 main_router = APIRouter()
+main_router.include_router(charity_project_router, prefix='/charity_project', tags=['Charity Projects'])
 main_router.include_router(user_router)
