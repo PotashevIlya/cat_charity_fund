03b192de81214372bd2e7d9e60be2f6f840d2043
Илья Поташев <potaschev.ilya@yandex.ru>
2025-01-16 00:40:41 +0300

    reworked investment_logic

--- app/services/investment_logic.py
+++ app/services/investment_logic.py
@@ -1,19 +1,12 @@
 from datetime import datetime
 
-
-
-
-
-
 from app.models import CharityProjectAndDonationBaseModel
 
 
-
-
 def distribute_investments(
-    target: CharityProjectAndDonationBaseModel, 
+    target: CharityProjectAndDonationBaseModel,
     sources: list[CharityProjectAndDonationBaseModel]
-    ) -> tuple[CharityProjectAndDonationBaseModel]:
+) -> tuple[CharityProjectAndDonationBaseModel]:
     for source in sources:
         current_sum = source.full_amount - source.invested_amount
         required_sum = target.full_amount - target.invested_amount
@@ -28,14 +21,8 @@ def distribute_investments(
                 obj.fully_invested = True
                 obj.close_date = datetime.utcnow()
         else:
+            source.invested_amount = source.full_amount
             source.fully_invested = True
             source.close_date = datetime.utcnow()
             target.invested_amount += current_sum
     return target, sources
-                    
-
-
-        
-
-
-
