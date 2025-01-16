16c44a028e181ad2d38b02f08435c5c1083b6d94
Илья Поташев <potaschev.ilya@yandex.ru>
2025-01-16 21:14:58 +0300

    version 4.1

--- app/services/investment_logic.py
+++ app/services/investment_logic.py
@@ -7,17 +7,18 @@ def distribute_investments(
     target: InvestmentBaseModel,
     sources: list[InvestmentBaseModel]
 ) -> list[InvestmentBaseModel]:
-    changed_objects = []
+    changed = []
     for source in sources:
-        ready_to_accept = target.full_amount - target.invested_amount
-        if ready_to_accept == 0:
-            break
-        ready_to_give = source.full_amount - source.invested_amount
-        trans_amount = min(ready_to_accept, ready_to_give)
+        trans_amount = min(
+            target.full_amount - target.invested_amount,
+            source.full_amount - source.invested_amount
+        )
         for obj in (target, source):
             obj.invested_amount += trans_amount
             if obj.invested_amount == obj.full_amount:
                 obj.fully_invested = True
                 obj.close_date = datetime.utcnow()
-            changed_objects.append(obj)
-    return changed_objects
+            changed.append(source)
+        if target.full_amount - target.invested_amount == 0:
+            break
+    return changed
